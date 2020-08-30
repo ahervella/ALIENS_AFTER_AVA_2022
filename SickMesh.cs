e1e47261c0a05714ca7017ba89eea1a17bd0c765
Alejandro <ahervella@me.com>
2020-08-30 16:41:11 -0400

    cleaned up!
    -decided to take out unit shit cause probably won't need it now
    - gonna work on horizontal wrapping before making more terrains

--- SickMesh.cs
+++ SickMesh.cs
@@ -13,6 +13,7 @@ public class SickMesh : MonoBehaviour
 
     public bool drawShapes = true;
     public bool drawPoints = true;
+    public bool drawOnlyControlPoints = false;
 
     //control point height and width
     public int height = 10;
@@ -29,19 +30,19 @@ public class SickMesh : MonoBehaviour
 
     int[] triangleRenIndices;
 
-    //actual render height and width after applying fill points
+    //actual render height and width with repsect to a unit
+    //of 1 fill shape length or width
     int rendHeight;
     int rendWidth;
 
     //length between two control points (fill points will adjust to this)
-    public float heightUnit = 1;
-    public float widthUnit = 1;
+    //public float heightUnit = 1;
+    //public float widthUnit = 1;
 
     //distance between rendered vertecies
-    float xRendVertDist;
-    float yRendVerDist;
+    //float xRendVertDist;
+    //float yRendVerDist;
 
-    bool blah = false;
 
     Vector3[] vertices;
 
@@ -59,32 +60,14 @@ public class SickMesh : MonoBehaviour
 
         rendHeight = height + (height * fillPointCount);
         rendWidth = width + (width * fillPointCount);
-        xRendVertDist = widthUnit / (fillPointCount + 1);
-        yRendVerDist = heightUnit / (fillPointCount + 1);
+        //xRendVertDist = widthUnit / (fillPointCount + 1);
+        //yRendVerDist = heightUnit / (fillPointCount + 1);
 
-        CreateRendVerticies();
-        CreateVetices();
-        CreateRendShapes();
-
+        createControlVertices();
         refereshRenderedPoints();
-        //applyTestTerrain();
-
-        //applyTerrain2(testTerrain);
-
-
-        //applyTerrain(testTerrain);
     }
 
-    private void Update()
-    {
-        generateTerrain();
-        moveMesh();
-        rendUpdateMesh();
-    }
-
-
-
-    void CreateVetices()
+    void createControlVertices()
     {
         vertices = new Vector3[(height + 1) * (width + 1)];
 
@@ -99,277 +82,6 @@ public class SickMesh : MonoBehaviour
 
     }
 
-    void CreateRendVerticies()
-    {
-        rendVertices = new Vector3[(rendHeight + 1) * (rendWidth + 1)];
-
-        for (int i = 0, vertIndex = 0; i <= rendHeight; i++)
-        {
-            for (int k = 0; k <= rendWidth; k++)
-            {
-                //need to do - i so that it goes top down
-                rendVertices[vertIndex] = new Vector3(k * xRendVertDist, 0, (rendHeight - i) * yRendVerDist);
-                vertIndex++;
-
-            }
-
-
-        }
-    }
-
-
-    void CreateRendShapes()
-    {
-        triangleRenIndices = new int[rendHeight * rendWidth * 6];
-        int vert = 0;
-        int triIndex = 0;
-
-        for (int i = 0; i < rendHeight; i++)
-        {
-            for (int k = 0; k < rendWidth; k++)
-            {
-                //verticies are from right to left, and moves a row down
-
-                //draws from top left to bottom right, clockwise
-                triangleRenIndices[triIndex] = vert;
-                triangleRenIndices[triIndex + 1] = vert + 1;
-                //need to add 2 because moved over a column (one extra vertice per column)
-                triangleRenIndices[triIndex + 2] = vert + rendWidth + 2;
-
-
-                //draws from bottom right to top left, clockwise
-                triangleRenIndices[triIndex + 3] = vert + rendWidth + 2;
-                triangleRenIndices[triIndex + 4] = vert + rendWidth + 1;
-                triangleRenIndices[triIndex + 5] = vert;
-                vert++;
-                triIndex += 6;
-
-            }
-            vert++;
-        }
-    }
-
-
-
-    private void OnDrawGizmos()
-    {
-        if (rendVertices == null || !drawPoints) { return; }
-
-
-        for (int i = 0; i < rendVertices.Length; i++)
-        {
-            Vector3 point = rendVertices[i];
-            if (point.z <= height - topMargin
-                && point.z >= bottomMargin
-                && point.x <= width - rightMargin
-                && point.x >= leftMargin)
-            {
-                Gizmos.DrawSphere(point, 0.05f);
-            }
-            
-        }
-
-
-        return;
-        for (int k = 0; k < vertices.Length; k++)
-        {
-            Gizmos.DrawSphere(vertices[k], 0.05f);
-        }
-        
-    }
-
-    void applyTestTerrain()
-    {
-        if (testTerrain == null) { return; }
-
-        if (testTerrain.terrainData.Length == 0) { return; }
-
-        testTerrain.reCalcVertecies(fillPointCount);
-
-        int offset = 0;
-        float positionOffset = fillPointCount == 0 ? 0 : 1f / (float)(fillPointCount + 1) * fillPointCount;
-
-        Vector3[] offSetTerrainData = testTerrain.offSetData(height - testTerrain.height + positionOffset);
-
-        for (int i = 0; i < testTerrain.rendHeight; i++)
-        {
-
-            for (int k = 0; k < testTerrain.rendWidth; k++)
-            {
-                int vertIndex = i * rendWidth + k + offset;
-                int terrainIndex = i * testTerrain.rendWidth + k;
-
-                Vector3 nonUnitAdjusted = offSetTerrainData[terrainIndex];
-                rendVertices[vertIndex] = new Vector3(widthUnit * nonUnitAdjusted.x, nonUnitAdjusted.y, heightUnit * nonUnitAdjusted.z);
-            }
-            offset++;
-        }
-    }
-
-
-    void applyTerrain2(Terrainnnn terrain)
-    {
-        int offset = 0;
-
-        Vector3 heightOffset = new Vector3(0, 0, vertices[0].z - terrain.height + 1);
-
-        //here not <= because terrain heights are the vertecie count
-        for (int i = 0; i < terrain.height; i++)
-        {
-            for (int k = 0; k < terrain.width; k++)
-            {
-                int vertIndex = i * width + k + offset;
-                int terrainIndex = i * terrain.width + k;
-                float actualElevation = Mathf.Max(vertices[vertIndex].y, (terrain.terrainData[terrainIndex] + heightOffset).y);
-                vertices[vertIndex] = terrain.terrainData[terrainIndex] + heightOffset;
-                vertices[vertIndex] = new Vector3(vertices[vertIndex].x, actualElevation, vertices[vertIndex].z);
-            }
-            offset++;
-        }
-
-        refereshRenderedPoints();
-    }
-
-    void applyTerrain(Terrainnnn terrain)
-    {
-        terrain.reCalcVertecies(fillPointCount);
-
-        int offset = 0;
-        float positionOffset = fillPointCount == 0 ? 0 : 1f / (float)(fillPointCount + 1) * fillPointCount;
-        //positionOffset = 
-
-        Vector3[] offSetTerrainData = terrain.offSetData(height - terrain.height + positionOffset);
-
-        for (int i = 0; i < terrain.rendHeight; i++)
-        {
-
-            for (int k = 0; k < terrain.rendWidth; k++)
-            {
-                int vertIndex = i * rendWidth + k + offset;
-                int terrainIndex = i * terrain.rendWidth + k;
-
-                Vector3 nonUnitAdjusted = offSetTerrainData[terrainIndex];
-                rendVertices[vertIndex] = new Vector3(widthUnit * nonUnitAdjusted.x, nonUnitAdjusted.y, heightUnit * nonUnitAdjusted.z);
-            }
-            offset++;
-        }
-    }
-
-
-    void rendUpdateMesh()
-    {
-
-       
-
-        mesh.vertices = rendVertices;
-        if (drawShapes) { mesh.triangles = triangleRenIndices; }
-
-        CreateRendShapes();
-        mesh.RecalculateNormals();
-    }
-
-
-    void generateTerrain()
-    {
-        if (blah) { return; }
-        int terrainSelector = Mathf.FloorToInt(Random.Range(0, terrains.Length-1));
-        float likelihoodSelector = Random.Range(0.00001f, 1f);
-
-        Terrainnnn selectedTerrain = terrains[terrainSelector];
-
-        if (likelihoodSelector <= selectedTerrain.appearanceLikelihood)
-        {
-            //applyTerrain(selectedTerrain);
-            applyTerrain2(selectedTerrain);
-        }
-    }
-
-
-    void moveMesh()
-    {
-
-        for (int i = 0; i < rendVertices.Length; i++)
-        {
-            rendVertices[i] += new Vector3(0, 0, -treadmillSpeed);
-        }
-
-        for (int k = 0; k < vertices.Length; k++)
-        {
-            vertices[k] += new Vector3(0, 0, -treadmillSpeed);
-        }
-
-        //incase points move so much that need to move rows more than once
-        while (applyTreadmillEffect()) { }
-        refereshRenderedPoints();
-    }
-
-
-    bool applyTreadmillEffect()
-    {
-
-        if (vertices[0].z < (height - 1))
-        {
-            Vector3[] shortenedList = new Vector3[vertices.Length - (width + 1)];
-
-            for (int i = 0; i < shortenedList.Length; i++)
-            {
-                shortenedList[i] = vertices[i];
-            }
-
-            for (int k = 0; k < vertices.Length; k++)
-            {
-                if (k <= width)
-                {
-                    vertices[k] = new Vector3(k, 0, shortenedList[0].z + 1);
-                }
-                else
-                {
-                    vertices[k] = shortenedList[k - width - 1];
-                }
-            }
-            return true;
-        }
-
-        return false;
-
-
-
-
-        if (rendVertices[0].z < (rendHeight-1) * heightUnit/(fillPointCount+1))
-        {
-
-            Vector3[] shortenedList = new Vector3[rendVertices.Length - (rendWidth + 1)];
-            for (int i = 0; i < shortenedList.Length; i++)
-            {
-                shortenedList[i] = rendVertices[i];
-            }
-
-
-           for (int k = 0; k < rendVertices.Length; k++)
-            {
-                //if first row of points
-                if (k <= rendWidth)
-                {
-                    //need to be placed properly behind first row, so use shortenedList[0].z + yRendVerDist
-                    rendVertices[k] = new Vector3(k * xRendVertDist, 0, shortenedList[0].z + yRendVerDist);
-                    
-                }
-                else
-                {
-                    rendVertices[k] = shortenedList[k - rendWidth - 1];
-                }
-                
-            }
-
-            return true;
-        }
-
-        return false;
-    }
-
-
-
-
 
 
     void refereshRenderedPoints()
@@ -377,7 +89,7 @@ public class SickMesh : MonoBehaviour
         int rvHeight = height + fillPointCount * (height);
         int rvWidth = width + fillPointCount * (width);
 
-        Vector3[] renderedVerts = new Vector3[(rvHeight+1) * (rvWidth+1)];
+        Vector3[] renderedVerts = new Vector3[(rvHeight + 1) * (rvWidth + 1)];
 
         //build list with full rows where control rows would be
         for (int i = 0, rowOffset = 0; i <= height; i++)
@@ -435,6 +147,7 @@ public class SickMesh : MonoBehaviour
 
 
 
+
     Vector3 smoothERP(Vector3 firstRefPoint, Vector3 secondRefPoint, int index)
     {
         Vector3 diff = secondRefPoint - firstRefPoint;
@@ -448,6 +161,176 @@ public class SickMesh : MonoBehaviour
 
     }
 
+
+
+
+
+    private void OnDrawGizmos()
+    {
+        if (rendVertices == null || !drawPoints) { return; }
+
+        Vector3[] points = drawOnlyControlPoints ? vertices : rendVertices;
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            Vector3 point = points[i];
+            if (point.z <= height - topMargin
+                && point.z >= bottomMargin
+                && point.x <= width - rightMargin
+                && point.x >= leftMargin)
+            {
+                Gizmos.DrawSphere(point, 0.05f);
+            }
+        }
+    }
+
+
+
+    private void Update()
+    {
+        generateTerrain();
+        moveMesh();
+        updateMesh();
+    }
+
+
+    void generateTerrain()
+    {
+        int terrainSelector = Mathf.FloorToInt(Random.Range(0, terrains.Length - 1));
+        float likelihoodSelector = Random.Range(0.00001f, 1f);
+
+        Terrainnnn selectedTerrain = terrains[terrainSelector];
+
+        if (likelihoodSelector <= selectedTerrain.appearanceLikelihood)
+        {
+            addTerrain(selectedTerrain);
+        }
+    }
+
+
+
+    void addTerrain(Terrainnnn terrain)
+    {
+        int offset = 0;
+
+        Vector3 heightOffset = new Vector3(0, 0, vertices[0].z - terrain.vertHeight + 1);
+
+        //here not <= because terrain heights are the vertecie count
+        for (int i = 0; i < terrain.vertHeight; i++)
+        {
+            for (int k = 0; k < terrain.vertWidth; k++)
+            {
+                int vertIndex = i * width + k + offset;
+                int terrainIndex = i * terrain.vertWidth + k;
+                float actualElevation = Mathf.Max(vertices[vertIndex].y, (terrain.terrainData[terrainIndex] + heightOffset).y);
+                vertices[vertIndex] = terrain.terrainData[terrainIndex] + heightOffset;
+                vertices[vertIndex] = new Vector3(vertices[vertIndex].x, actualElevation, vertices[vertIndex].z);
+            }
+            offset++;
+        }
+
+        refereshRenderedPoints();
+    }
+
+
+
+    void moveMesh()
+    {
+
+        for (int i = 0; i < rendVertices.Length; i++)
+        {
+            rendVertices[i] += new Vector3(0, 0, -treadmillSpeed);
+        }
+
+        for (int k = 0; k < vertices.Length; k++)
+        {
+            vertices[k] += new Vector3(0, 0, -treadmillSpeed);
+        }
+
+        //incase points move so much that need to move rows more than once
+        while (applyTreadmillEffect()) { }
+        refereshRenderedPoints();
+    }
+
+
+
+    bool applyTreadmillEffect()
+    {
+
+        if (vertices[0].z < (height - 1))
+        {
+            Vector3[] shortenedList = new Vector3[vertices.Length - (width + 1)];
+
+            for (int i = 0; i < shortenedList.Length; i++)
+            {
+                shortenedList[i] = vertices[i];
+            }
+
+            for (int k = 0; k < vertices.Length; k++)
+            {
+                if (k <= width)
+                {
+                    vertices[k] = new Vector3(k, 0, shortenedList[0].z + 1);
+                }
+                else
+                {
+                    vertices[k] = shortenedList[k - width - 1];
+                }
+            }
+            return true;
+        }
+
+        return false;
+
+    }
+
+
+
+
+    void updateMesh()
+    {
+        mesh.vertices = rendVertices;
+
+        if (drawShapes)
+        {
+            createRendShapes();
+            mesh.triangles = triangleRenIndices;
+            mesh.RecalculateNormals();
+        }
+    }
+
+
+
+    void createRendShapes()
+    {
+        triangleRenIndices = new int[rendHeight * rendWidth * 6];
+        int vert = 0;
+        int triIndex = 0;
+
+        for (int i = 0; i < rendHeight; i++)
+        {
+            for (int k = 0; k < rendWidth; k++)
+            {
+                //verticies are from right to left, and moves a row down
+
+                //draws from top left to bottom right, clockwise
+                triangleRenIndices[triIndex] = vert;
+                triangleRenIndices[triIndex + 1] = vert + 1;
+                //need to add 2 because moved over a column (one extra vertice per column)
+                triangleRenIndices[triIndex + 2] = vert + rendWidth + 2;
+
+
+                //draws from bottom right to top left, clockwise
+                triangleRenIndices[triIndex + 3] = vert + rendWidth + 2;
+                triangleRenIndices[triIndex + 4] = vert + rendWidth + 1;
+                triangleRenIndices[triIndex + 5] = vert;
+                vert++;
+                triIndex += 6;
+
+            }
+            vert++;
+        }
+    }
 
 }
 
--- Terrainnnn.cs
+++ Terrainnnn.cs
@@ -15,20 +15,16 @@ public class Terrainnnn : MonoBehaviour
 
     public Vector3[] terrainData;
 
-    //control point heights and widths
-    public int width = 0;
-    public int height = 0;
+    //control point vertecie Heights and Widths
+    public int vertWidth = 0;
+    public int vertHeight = 0;
 
     //multiplyer for text file floats (so user can use large numbers
     //for precision in text file)
     public float elevationMultiplyer = 0.05f;
 
-    //actual render widths and heights (after adding fill points)
-    public int rendWidth = 0;
-    public int rendHeight = 0;
 
-    //the direct pased data from the text file in the readable order
-    List<float[]> parsedData = new List<float[]>();
+
 
     private void OnEnable()
     {
@@ -38,6 +34,10 @@ public class Terrainnnn : MonoBehaviour
 
     public void parseTerrainFile()
     {
+        //the direct pased data from the text file in the readable order
+        List<float[]> parsedData = new List<float[]>();
+
+
         if (terrainFile == null) { return; }
 
         StreamReader sr = new StreamReader(terrainFolderPath + terrainFile.name + ".txt");
@@ -60,127 +60,26 @@ public class Terrainnnn : MonoBehaviour
         }
 
 
-        height = parsedData.Count;
-        width = parsedData[0].Length;
-        terrainData = new Vector3[width * height];
-
-        reCalcVertecies(0);
-
-    }
+        vertHeight = parsedData.Count;
+        vertWidth = parsedData[0].Length;
+        terrainData = new Vector3[vertWidth * vertHeight];
 
 
-
-    public void reCalcVertecies(int fillPointCount)
-
-    {
-        int rendPDHeight = parsedData.Count + fillPointCount * (parsedData.Count - 1);
-        int rendPDWidth = parsedData[0].Length + fillPointCount * (parsedData[0].Length - 1);
-
-        float[][] rendParsedData = new float[rendPDHeight][];
-        for (int blah = 0; blah < rendParsedData.Length; blah++) { rendParsedData[blah] = new float[rendPDWidth]; }
-
-        //build list with empty spots from fillPoint count
-        for (int g = 0, rowOffset = 0; g < parsedData.Count; g++)
-        {
-            rendParsedData[g + rowOffset] = (new float[parsedData[g].Length + (fillPointCount * (parsedData[g].Length -1))]);
-
-            for (int k = 0, colOffset = 0; k < parsedData[g].Length; k++)
-            {
-                rendParsedData[g + rowOffset][k + colOffset] = parsedData[g][k];
-
-                colOffset += fillPointCount;
-            }
-
-            rowOffset += fillPointCount;
-
-        }
-
-        //calculate and populate in between empty spots between rows that already have stuff
-        for (int l = 0; l < rendParsedData.Length; l++)
-        {
-            //if not one of the original control points (aka still needs a value), next!
-            if (l % (fillPointCount+1) != 0) { continue; }
-
-
-            for (int ll = 0; ll < rendParsedData[l].Length; ll++)
-            {
-                //basically saying: if first non control point...
-                //basically finds the first one
-                if (ll % (fillPointCount + 1) != 0)
-                {
-                    float firstVal = rendParsedData[l][ll - 1];
-                    float secondVal = rendParsedData[l][ll + fillPointCount];
-                    float diff = secondVal - firstVal;
-
-                    float sinRadAng = (Mathf.PI) / (float)(fillPointCount + 1);
-                    
-                    for (int u = 0; u < fillPointCount; u++)
-                    {
-                        float theta = sinRadAng * (u + 1);
-                        float curveMultiplyer = Mathf.Sin(theta - Mathf.PI / 2f) * 0.5f + 0.5f;
-                        rendParsedData[l][ll + u] = firstVal + diff * curveMultiplyer;
-                    }
-
-                    //to jump to next section to fill
-                    ll += fillPointCount;
-                }
-            }
-            
-        }
-
-
-        //populate empty columns between full rows
-        for (int k = 0; k < rendParsedData.Length; k++)
-        {
-            //if a control point, next! We need to only iterate empty rows
-            //basically finds the first one
-            if (k % (fillPointCount + 1) == 0) { continue; }
-
-            for (int kk = 0; kk < fillPointCount; kk++)
-            {
-                for (int jjj = 0; jjj < rendParsedData[k].Length; jjj++)
-                {
-                    float firstVal = rendParsedData[k - 1][jjj];
-                    float secondVal = rendParsedData[k + fillPointCount][jjj];
-                    float diff = secondVal - firstVal;
-
-                    float sinRadAng = (Mathf.PI) / (float)(fillPointCount + 1);
-                    float theta = sinRadAng * (kk + 1);
-                    float curveMultiplyer = Mathf.Sin(theta - Mathf.PI / 2f) * 0.5f + 0.5f;
-
-                    rendParsedData[k + kk][jjj] = firstVal + diff * curveMultiplyer;
-                }
-            }
-
-            //to jump to next section to fill
-            k += fillPointCount;
-
-        }
-
-
-
-
-        rendHeight = rendParsedData.Length;
-        //each row of the text file should have the same width
-        rendWidth = rendParsedData[0].Length;
-
-        terrainData = new Vector3[rendWidth * rendHeight];
-
-        float multiplyer = 1f / (fillPointCount + 1);
         //convert depth string vals and location to Vector3 val
-        for (int i = 0; i < rendParsedData.Length; i++)
+        for (int i = 0; i < vertHeight; i++)
         {
-            
-            for (int k = 0; k < rendParsedData[i].Length; k++)
-            {
-                float heightVal = rendHeight - i - 1;
-                float depthVal = rendParsedData[i][k] * elevationMultiplyer;
 
-                terrainData[i * rendWidth + k] = new Vector3(k * multiplyer, depthVal, heightVal * multiplyer);
+            for (int k = 0; k < vertWidth; k++)
+            {
+                float vertHeightVal = vertHeight - i - 1;
+                float depthVal = parsedData[i][k] * elevationMultiplyer;
+
+                terrainData[i * vertWidth + k] = new Vector3(k, depthVal, vertHeightVal);
             }
         }
 
         //terrain data indecies are stored from left to right, going down
+
 
     }
 
