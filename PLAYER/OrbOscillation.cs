507daf2099734dd0c2583afa8775e2f2a6202000
Alejandro Hervella <>
2022-03-02 15:53:21 -0500

    - drew orb turret, added to scene, and made it oscillate!

---
+++ PLAYER/OrbOscillation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbOscillation : MonoBehaviour
+{
+    //TODO: configure this and the settings script to have more dynamic
+    //movements for when the player gets hit or when we fire a projectile;
+
+    [SerializeField]
+    private SO_OrbOscillationSettings settings;
+
+    private Coroutine oscillationCR = null;
+    private float oscillationTweenPerc = 1f;
+    private Vector3 cachedOGLocalPosition = default;
+
+    private void Awake()
+    {
+        cachedOGLocalPosition = transform.localPosition;
+        StartOscillation();
+    }
+
+    private void StartOscillation()
+    {
+        if (oscillationCR != null)
+        {
+            StopCoroutine(oscillationCR);
+        }
+
+        oscillationCR = StartCoroutine(OscillationCoroutine());
+    }
+
+    private IEnumerator OscillationCoroutine()
+    {
+        while (true)
+        {
+            oscillationTweenPerc += Time.deltaTime / settings.OscillationTime;
+            oscillationTweenPerc %= 1f;
+            float yDelta = Mathf.Sin(oscillationTweenPerc * 2 * Mathf.PI) * settings.OscillationDisplacement;
+            transform.localPosition = cachedOGLocalPosition + new Vector3(0, yDelta, 0);
+            yield return null;
+        }
+    }
+}
---
+++ SCRIPTABLE_OBJECTS/SO_OrbOscillationSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SO_OrbOscillationSettings", menuName = "ScriptableObjects/StaticData/SO_OrbOscillationSettings")]
+public class SO_OrbOscillationSettings : ScriptableObject
+{
+    [SerializeField]
+    private float oscillationDisplacement = 2f;
+    public float OscillationDisplacement => oscillationDisplacement;
+
+    [SerializeField]
+    private float oscillationTime = 1f;
+    public float OscillationTime => oscillationTime;
+}
