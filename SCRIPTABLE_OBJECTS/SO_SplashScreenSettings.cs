b7bbf3ed85bd90eee39f7fe8b69010d52e6f06d2
Alejandro Hervella <>
2022-04-21 22:59:14 -0400

    - made a better splash screen real quick hehe

---
+++ SCRIPTABLE_OBJECTS/SO_SplashScreenSettings.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "SO_SplashScreenSettings", menuName = "ScriptableObjects/StaticData/SO_SplashScreenSettings")]
+public class SO_SplashScreenSettings : ScriptableObject
+{
+    [SerializeField]
+    private List<SplashScreenWrapper> wrappers = new List<SplashScreenWrapper>();
+
+    public List<SplashScreenWrapper> GetOrderedSplashScreenWrappers()
+    {
+        wrappers.Sort((a, b) => a.Order.CompareTo(b.Order));
+        return wrappers;
+    }
+}
+
+[Serializable]
+public class SplashScreenWrapper
+{
+    [SerializeField]
+    private int order = 0;
+    public int Order => order;
+
+    [SerializeField]
+    private Sprite splashImage = null;
+    public Sprite SplashImage => splashImage;
+
+    [SerializeField]
+    private float duration = 3f;
+    public float Duration => duration;
+}
---
+++ UI/SplashScreenManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SplashScreenManager : MonoBehaviour
+{
+    [SerializeField]
+    private PSO_CurrentGameMode currGameMode = null;
+
+    [SerializeField]
+    private SO_SplashScreenSettings settings = null;
+
+    [SerializeField]
+    private Image splashImg = null;
+
+    private void Awake()
+    {
+        StartCoroutine(SplashScreenSequenceCR());
+    }
+
+    private IEnumerator SplashScreenSequenceCR()
+    {
+        foreach(SplashScreenWrapper ssw in settings.GetOrderedSplashScreenWrappers())
+        {
+            if (ssw.SplashImage == null)
+            {
+                splashImg.color = Color.black;
+            }
+            else
+            {
+                splashImg.sprite = ssw.SplashImage;
+                splashImg.color = Color.white;
+            }
+            
+            yield return new WaitForSeconds(ssw.Duration);
+        }
+
+        currGameMode.ModifyValue(GameModeEnum.MAINMENU);
+    }
+}
