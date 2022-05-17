3ffd97ab0ab3c4283989f57270d38abbf76cf86b
Alejandro Hervella <>
2022-05-17 12:00:28 -0400

    - made most of zone 2 static hazard art and imported into unity - setup prefabs with zone 2 - got a basic splash spawner working for water effect

---
+++ UTIL/SplashSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject splashPrefab = null;
+
+    [SerializeField]
+    private PSO_TerrainTreadmillNodes terrNodes = null;
+
+    [SerializeField]
+    private List<GameObject> orderedSpawnLocations = new List<GameObject>();
+
+    public void AE_SpawnSplash(int spawnLocRefIndex)
+    {
+        GameObject instance = Instantiate(splashPrefab, terrNodes.Value.VerticalNode);
+
+        Vector3 spawnPos = spawnLocRefIndex < orderedSpawnLocations.Count ?
+            orderedSpawnLocations[spawnLocRefIndex].transform.position
+            : transform.position;
+
+        instance.transform.position = spawnPos;//new Vector3(spawnPos.x, 0, spawnPos.z);
+    }
+}
