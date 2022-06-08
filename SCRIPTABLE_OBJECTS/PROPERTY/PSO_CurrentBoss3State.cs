e792a0d81f004b738ecf9398bf4a644a79439e05
Alejandro Hervella <>
2022-06-08 13:47:48 -0400

    - finished setting up boiler plate files for boss3 (first phase), now just more programming!

---
+++ SCRIPTABLE_OBJECTS/PROPERTY/PSO_CurrentBoss3State.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PSO_CurrentBoss3State", menuName = "ScriptableObjects/Property/PSO_CurrentBoss3State")]
+public class PSO_CurrentBoss3State : PropertySO<Boss3State>
+{
+    public override void ModifyValue(Boss3State mod)
+    {
+        if (Value != mod)
+        {
+            SetValue(mod);
+        }
+    }
+}
