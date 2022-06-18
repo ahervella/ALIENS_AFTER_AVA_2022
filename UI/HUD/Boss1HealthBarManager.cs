e26b2c11870b8c129a23d849ab47b2419b65be6b
Alejandro Hervella <>
2022-06-18 16:02:48 -0400

    - made boss health bar white flash

--- UI/HUD/Boss1HealthBarManager.cs
+++ UI/HUD/Boss1HealthBarManager.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Boss1HealthBarManager : AFillBarManager<PSO_FillBarQuant, SO_BossHealthBarSettings>
 {
+    [SerializeField]
+    private UISpriteFlasher healthBarFlasher = null;
+
     protected override void AfterAwake()
     {
     }
 
     protected override void AfterModifyCurrQuant(int oldQuant, int newQuant)
     {
+        if (oldQuant > newQuant)
+        {
+            healthBarFlasher.Flash();
+        }
     }
 
     protected override void AfterStart()
